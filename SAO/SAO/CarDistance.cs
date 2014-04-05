5b98cb34f9a58e868dad85ef193e76359cadd193
Lisu <tennlos@gmail.com>
2014-04-05 11:43:28 +0200

    Road class prepared

---
+++ SAO/SAO/CarDistance.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SAO
+{
+    public class CarDistance
+    {
+        public Car Car { get; set; }
+        public int Distance { get; set; }
+    }
+}
---
+++ SAO/SAO/Road.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SAO
+{
+    public class Road
+    {
+        //get later from Crossroads
+        public int Y { get; set; }
+        public int X { get; set; }
+        public int Length { get; set; }
+
+        //lesser coordinates 
+        public Crossroad First { get; set; }
+
+        public int IncreasingLaneCount { get; set; }
+        public int DecreasingLaneCount { get; set; }
+
+        //bigger coordinates
+        public Crossroad Second { get; set; }
+
+        public LinkedList<CarDistance>[] IncreasingLanes { get; set; }
+        public LinkedList<CarDistance>[] DecreasingLanes { get; set; }
+
+        public void AddCarToLinkedListWithParameterDistance()
+        {
+            
+        }
+    }
+}
