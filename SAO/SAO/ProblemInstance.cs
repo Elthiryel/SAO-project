cf49b4d475b601ec1ac0459538b9873af9ebced2
Lisu <tennlos@gmail.com>
2014-04-05 13:32:35 +0200

    ProblemInstance added

---
+++ SAO/SAO/ProblemInstance.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SAO
+{
+    public class ProblemInstance
+    {
+        public List<Road> Roads { get; set; }
+        public List<Crossroad> Crossroads { get; set; }
+        public List<Route> Routes { get; set; }
+
+        public static ProblemInstance GenerateInstance(string path)
+        {
+            return null;
+        }
+    }
+}
--- SAO/SAO/Route.cs
+++ SAO/SAO/Route.cs
@@ -9,6 +9,7 @@ namespace SAO
 	public class Route
 	{
 		public List<Road> Roads { get; set; }
+        public int Priority { get; set; }
 
 		// distance will be computed
 		public int Distance { get; set; }
